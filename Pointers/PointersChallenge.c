3e7aa96369f7522935d02ab068a0373b5998fbb6
Marsad-Zoardar <marsadzoardar@gmail.com>
2026-08-12 00:21:18 +0000

    Pointers

---
+++ Pointers/PointersChallenge.c
@@ -0,0 +1,24 @@
+//Author: Md Marsad Zoardar
+//August 12, 2026
+//Pointers challenge
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int myNumber = 1001;    //variable initialized 
+    int *pmyNumber = NULL;  //pointer initialized
+    
+    printf("The address of myNumber is: %p\n", &myNumber);
+    
+    pmyNumber = &myNumber;  //The address of the int variable is assigned to the pointer (It's a hex value)
+    
+    printf("The address of the pointer is: %p\n", (void*) &pmyNumber);  //The output should be the address of the pointer, a hex value
+    printf("The value of the pointer is: %p\n", pmyNumber);             //The output should be the address of the int variable, a hex value
+    printf("The value pointer is pointing to is: %d\n", *pmyNumber);    //The output should be the value of the int variable myNumber 1001
+    
+    
+    return 0;
+}
