e4e824a9f0b66ab3b48f8692eac82f235e7c7d91
Marsad-Zoardar <marsadzoardar@gmail.com>
2026-08-13 10:05:40 +0000

    Done with pointers

--- Pointers/DynamicMemoryAllocation.c
+++ Pointers/DynamicMemoryAllocation.c
@@ -32,23 +32,48 @@ int main(int argc, char **argv){
     //takes two arguments
     
     /*********************Example Program********************/
-    char *str = NULL;
+//    char *str = NULL;
+//    
+//    //initial memory allocation
+//    str = (char*) malloc(15*sizeof(char));
+//    strcpy(str, "Marsad");
+//    printf("String = %s, address = %p\n", str, str);
+//    
+//    
+//    //Reallocating memory
+//    str = (char*) realloc(str, 25*sizeof(char));
+//    strcat(str, " Zoardar");
+//    printf("String = %s, address = %p\n\n", str, str);
+//    
+//    free(str);
     
-    //initial memory allocation
-    str = (char*) malloc(15*sizeof(char));
-    strcpy(str, "Marsad");
-    printf("String = %s, address = %p\n", str, str);
+//    int *a;
+//    a = (int*)malloc(sizeof(int));
+//    *a = 100;
+//    printf("*a%d\n", *a);
+//    //free(a);
+//    
+//    a = (int*)malloc(sizeof(int));
+//    *a = 200;
+//    printf("a%p\n", a);
+//    *a = 200;
+//    printf("a%d\n", *a);
+//    
+        
+    char *p, *q;
+    p = (char*)malloc(3*sizeof(char));
+    q = p;
     
+    strcpy(p, "hello");
+    printf("p = %s", p);
+    printf("q = %s", q);
     
-    //Reallocating memory
-    str = (char*) realloc(str, 25*sizeof(char));
-    strcat(str, " Zoardar");
-    printf("String = %s, address = %p\n\n", str, str);
+    free(q);
     
-    free(str);
+    q = NULL;
     
-    
-    
+    printf("%s", p);
+    printf("%s", q);
     
     return 0;
 }
